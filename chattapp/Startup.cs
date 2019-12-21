9c6889fba437430cad1ddd31959ee70abddf6c3b
shawnzxx <shawnzhang.dev@gmail.com>
2019-12-21 13:57:26 +0800

    add app insght

--- chattapp/Startup.cs
+++ chattapp/Startup.cs
@@ -58,6 +58,7 @@ namespace chattapp
             //});
 
             services.AddSignalR().AddAzureSignalR(signalRConnectiongString);
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
